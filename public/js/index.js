f83c7d43af343d9cf464bc756f8fccaba5ab4586
Jashan <jashankhangura10@gmail.com>
2021-11-16 13:49:03 +0530

    Initial commit

---
+++ public/js/index.js
@@ -0,0 +1,32 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#p1')
+const message2 = document.querySelector('#p2')
+const message3 = document.querySelector('#p3')
+
+message1.textContent = ''
+message2.textContent = ''
+message3.textContent = ''
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+ 
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+    message3.textContent = ''
+
+    fetch('http://localhost:3000/weather?address='+ search.value)
+    .then((response) => {
+        response.json().then((data) => {
+         if(data.error) {
+             message1.textContent = data.error
+         }
+           else{
+            message1.textContent = 'Location: ' + data.location
+            message2.textContent = 'Temperature: ' + data.weather.Temperature
+            message3.textContent = 'Humidity: ' + data.weather.Humidity   
+        }
+        })
+    })
+    
+})
---
+++ src/app.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const request = require('request')
+const geoCode = require('../../Weather-app/utils/geoCode')
+const forecast = require('../../Weather-app/utils/forecast')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname,'../templates/views')
+const partialPath = path.join(__dirname,'../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Jashan'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({error: 'Provide an address.'})
+    }
+    else {
+        geoCode(req.query.address, (error, {latitude, longitude, location} = {} ) => {
+        
+            if (error) {
+                return res.send({error: 'An error occured.'})
+            }
+    
+            forecast(latitude, longitude, (error, forecastData) => {
+               
+               if (error) {
+                return res.send({error: 'An error occured.'})
+               }
+                   res.send({
+                       weather: forecastData,
+                       location,
+                       address: req.query.address
+                   })
+            })
+        })
+    }  
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Jashan'
+    })
+})
+
+app.get('/product', (req, res) => {
+    if(!req.query.search) {
+      return res.send({
+            error: 'You must provide a search item'
+        })
+    }
+     res.send({
+        products: []
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'need help?',
+        name: 'Jashan'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('err2', {
+        title:"Help article not found"
+    })
+    })
+    
+app.get('*', (req, res) => {
+res.render('err', {
+    title:"Page not found"
+})
+})
+
+app.listen(3000, () => {
+    console.log('Server is up and running on port 3000.')
+})
